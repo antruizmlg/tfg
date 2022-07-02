e935beadc0c1838a5ecc1b48c95c0b7ab7bf4d8e
Antonio <antonioruizvalverde@uma.es>
2022-07-02 19:19:55 +0200

    classes added

---
+++ Hiperarista.py
@@ -0,0 +1,7 @@
+class Hiperarista:
+    def __init__(self, src, dest, accion, prob, coste):
+        self.source = src
+        self.destino = dest
+        self.accion = accion
+        self.prob = prob
+        self.coste = coste
---
+++ Hipergrafo.py
@@ -0,0 +1,4 @@
+class Hipergrafo:
+    def __init__(self, nodos, hiperaristas):
+        self.nodos = nodos
+        self.hiperaristas = hiperaristas 
