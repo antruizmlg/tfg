50d9376c6402066dbe189aa984dbadd82006245e
Antonio <antonioruizvalverde@uma.es>
2022-07-03 16:34:19 +0200

    changes on some funct

--- Hipergrafo.py
+++ Hipergrafo.py
@@ -3,6 +3,14 @@ class Hipergrafo:
         self.estados = estados
         self.hiperaristas = hiperaristas
 
+    def sucesores(self, estado):
+        sucesores = [] # Inicializamos la lista de sucesores
+        for a in self.hiperaristas: # Por cada hiperarista del hipergrafo.
+            if a.source == estado: # Si el origen de la hiperarista coincide con el estado
+                for elem in a.destino:
+                    sucesores.append(elem) # Introducimos en la lista de sucesores todos los nodos destinos del hiperarista.
+        return list(dict.fromkeys(sucesores)) # Devolvemos la lista de sucesores sin elementos repetidos
+
     def interseccion(self, listaEstados):
         estadosNuevoG = [] # Inicializamos los nodos del grafo intersección
         aristasNuevoG = [] # Inicializamos las aristas del grafo intersección.
@@ -12,3 +20,9 @@ class Hipergrafo:
         for ha in self.hiperaristas: # Para cada hiperarista del hipergrafo
             aristasNuevoG.append(ha.aristaConSubconjuntoDeNodos(listaEstados)) # Obtengo la hiperarista solo con los nodos en la lista de estados (intersección)
         return Hipergrafo(estadosNuevoG, aristasNuevoG)
+
+    def obtenerEstadoNoTerminal(self):
+        for e in self.estados:
+            if e.esTerminal():
+                return e
+        return None
--- LAO.py
+++ LAO.py
@@ -12,3 +12,10 @@ class LAO:
         I = []
         G = Hipergrafo([self.eIni], None)
         GV = Hipergrafo([self.eIni], None)
+        s = self.hipergrafo.interseccion(F).obtenerEstadoNoTerminal()
+        while s is not None:
+            F.remove(s) # Eliminamos el estado s
+            for estado in self.hipergrafo.sucesores(s): # Por cada sucesor de s en el hipergrafo
+                if estado not in I: # Si el sucesor no se encuentra en el conjunto I
+                    F.append(estado) # Lo introducimos en el conjunto F
+            I.append(s) # Introducimos s en el conjunto I
