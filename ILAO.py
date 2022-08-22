3be6c00852741f48581026dba9d5cdeeef636eb9
Antonio <antonioruizvalverde@uma.es>
2022-08-22 16:48:17 +0200

    ilao working

--- ILAO.py
+++ ILAO.py
@@ -22,7 +22,7 @@ class ILAO:
         # Inicialización grafo explícito y grafo solución
         envelope_graph = Graph({self.s0: []}, self.hg.dict_state)
         bpsg = Graph({self.s0: []}, self.hg.dict_state)
-        bpsg_states = filter(lambda s:not bpsg.dict_state[s].terminal, bpsg.states.keys())
+        bpsg_states = set(filter(lambda s:not bpsg.dict_state[s].final, bpsg.states.keys()))
 
         # Instanciación objeto algoritmo para su posterior ejecución en cada iteración
         if self.algorithm == 'PI':
@@ -30,32 +30,40 @@ class ILAO:
         if self.algorithm == 'VI':
             algorithm = Value_Iteration(self.hg, self.p, self.V)
 
-        while fringe & bpsg_states:
+        while True:
+            old_policy = deepcopy(self.p)
+
             stack_DFS = []
-            self.depth_first_search(envelope_graph, bpsg, self.s0, fringe, interior, stack_DFS)
-            self.update_values(bpsg, stack_DFS)
+            fringe = self.depth_first_search(envelope_graph, bpsg, self.s0, fringe, interior, stack_DFS)
+            self.update_values(envelope_graph, stack_DFS)
             algorithm.run(bpsg.states.keys())
-            bpsg_states = self.hg.get_bpsg_states(envelope_graph, self.p, set(), self.s0)
+
+            if all(old_policy[s] == self.p[s] for s in old_policy.keys()): # Si llegamos a convergencia, salimos del bucle
+                break
+
+        print(len(interior))
 
     def depth_first_search(self, envelope, bpsg, i, fringe, interior, stack):
-        stack.append(suc)
+        stack.append(i)
         if i in fringe:
             interior.add(i)
             fringe.remove(i)
-            fringe = fringe + filter(lambda s:not bpsg.dict_state[s].terminal and s not in interior, self.hg.get_successors(i))
+            fringe = fringe | set(filter(lambda s:not bpsg.dict_state[s].final and s not in interior, self.hg.get_successors(i)))
             envelope.states[i] = self.hg.states[i]
         else:
-            for suc in bpsg.get_connector(i, self.p[i]).probs.keys():
-                self.depth_first_search(bpsg, suc, stack)
+            for suc in self.hg.get_connector(i, self.p[i]).probs.keys():
+                if suc not in stack and not self.hg.dict_state[suc].final:
+                    fringe = self.depth_first_search(envelope, bpsg, suc, fringe, interior, stack)
+        return fringe
 
-    def update_values(self, bpsg, stack):
+    def update_values(self, graph, stack):
         actions = {'N', 'S', 'E', 'O'}
         while stack:
             s = stack.pop()
-            if not bpsg.dict_state[s].final:
+            if not graph.dict_state[s].final:
                 minimum = float('inf')
                 for a in actions:
-                    c = self.hg.get_connector(s, a)
+                    c = graph.get_connector(s, a)
                     val = c.cost
                     for p in c.probs.keys():
                         val += c.probs[p] * self.V[p]
